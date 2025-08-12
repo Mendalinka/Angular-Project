1e2e333a7ce708af15b00f08a83288f5f0e0fce6
Mendalinka <swatoslaw05.12@gmail.com>
2025-08-12 18:50:51 +0300

    add some components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CartComponent } from './Component/cart/cart.component';
+import { HeaderComponent } from './Component/header/header.component';
+import { MainComponent } from './Component/main/main.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CartComponent,HeaderComponent,MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  
+  title = 'Angular-Project';
 }
